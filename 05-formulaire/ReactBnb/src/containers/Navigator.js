3dad7071b5df2a07319f01bfeef27dfbc8e194ec
Victor Sabatier <victor@reactivic.com>
2020-10-09 10:50:06 +0200

    Update Navigator.js

--- 05-formulaire/ReactBnb/src/containers/Navigator.js
+++ 05-formulaire/ReactBnb/src/containers/Navigator.js
@@ -1,4 +1,4 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator } from '@react-navigation/stack';
 
 import HousingList from "./HousingList";
 import HousingDetail from "./HousingDetail";
