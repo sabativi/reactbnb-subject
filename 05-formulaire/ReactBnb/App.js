36abd7a9b73e14290cd16f2594a9659127240d5e
Victor Sabatier <victor@reactivic.com>
2020-10-09 10:50:40 +0200

    Update App.js

--- 05-formulaire/ReactBnb/App.js
+++ 05-formulaire/ReactBnb/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Navigator from "./src/containers/Navigator";
+import { NavigationContainer } from '@react-navigation/native';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <NavigationContainer>
         <Navigator />
+      </NavigationContainer>
       </View>
     );
   }
